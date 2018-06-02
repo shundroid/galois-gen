4fb08cf470a4722bc586ea7765e33c2207433889
shundroid <shundroidk@gmail.com>
2018-06-02 22:11:02 +0900

    added link to github

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,7 +2,20 @@
   .title
     v-toolbar(color="primary")
       v-toolbar-title ガロア理論ジェネレーター
+      v-spacer
+      v-toolbar-items.hidden-sm-and-down
+        v-btn(flat @click="goGithub") View Source Code
 </template>
+
+<script>
+export default {
+  methods: {
+    goGithub () {
+      location.href = 'https://github.com/shundroid/galois-gen'
+    }
+  }
+}
+</script>
 
 <style scoped>
 .title {
